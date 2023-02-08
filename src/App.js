a926395c4d89fafa1fa6ba76f8a9ae6e50ef4926
Marina Knysh <mrnknsh@gmail.com>
2023-02-08 22:31:51 +0300

    ToDo-1: Created field with adding task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import {CheckList} from "./views/components/checkList";
+import {AddTask} from "./views/components/addTask";
 
 function App() {
-  return (
-    <div className="App">
-      <CheckList/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>SMART TODO</h1>
+            <AddTask/>
+            <CheckList/>
+        </div>
+    );
 }
 
 export default App;
--- src/views/components/addTask/index.js
+++ src/views/components/addTask/index.js
@@ -1,3 +1,10 @@
-const AddTask = () =>{
+import './style.scss'
 
+export const AddTask = () => {
+    return (
+    <div className={'adding-wrapper'}>
+        <input type="text" className={'adding-field'}/>
+        <button className={'btn-add-task'}>Add</button>
+    </div>
+    )
 }
